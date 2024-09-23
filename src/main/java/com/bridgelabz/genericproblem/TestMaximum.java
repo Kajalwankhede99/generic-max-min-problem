8a6a2337a6b160466a3fbfb0e8526a4234dedcd1
Kajalwankhede99 <kajaldilipwankhede123@gmail.com>
2024-09-23 17:22:29 +0530

    Added: UC2 Given 3 floats find the maximum float

--- src/main/java/com/bridgelabz/genericproblem/TestMaximum.java
+++ src/main/java/com/bridgelabz/genericproblem/TestMaximum.java
@@ -2,9 +2,9 @@ package com.bridgelabz.genericproblem;
 
 public class TestMaximum
 {
-    public static void getMaxInteger(Integer[] arr) {
+    public static void getMaxFloat(Float[] arr) {
 
-        int max = arr[0];
+        float max = arr[0];
         int position = 0;
         for (int i = 0; i < arr.length; i++) {
             int a = arr[i].compareTo(max);
@@ -13,10 +13,10 @@ public class TestMaximum
                 position = i;
             }
         }
-        System.out.println("Maximum integer is " + max+" and its position is "+position);
+        System.out.println("Maximum float is " + max+" and its position is "+position);
     }
     public static void main(String[] args) {
-        Integer[] arr = {80, 30, 90};
-        getMaxInteger(arr);
+        Float[] arr = {2.3f, 7.8f, 3.4f};
+        getMaxFloat(arr);
     }
 }
